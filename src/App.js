6dfce992c96f302e4e69d2305be7b28ffb968f6f
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-10 19:43:56 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import Form from "./components/Form";
+import Form from "./components/Parent";
 
 function App() {
-  return <Form />;
+  
+  return <Parent />;
 }
 
 export default App;
